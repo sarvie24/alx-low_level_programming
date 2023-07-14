47f98209a9fad614c5933e5e6a65a4c6b058e6c4
samuel nnah <samuelnnah080@gmail.com.com>
2023-07-14 16:39:17 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
- char ch;
+char ch;
 
- for (ch = 'a'; ch <= 'z'; ch++)
- {
-  putchar(ch);
- }
- putchar('\n');
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
 
- return (0);
+return (0);
 }
