6fc8813d46f4fd4511226f59ab34bbaa01d3d264
samuel nnah <samuelnnah080@gmail.com.com>
2023-07-13 18:27:25 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program for printing line with printf function
+ * Return: 0 (works)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (o);
+}
+
