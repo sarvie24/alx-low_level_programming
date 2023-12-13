e8c0b4facbd831c3a93a783858a17876593fed1a
sarvie <samuelnnah080@gmail.com>
2023-12-13 07:02:46 -0800

    file to create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/all-funct.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * return: addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two integers
+ * @a: first int
+ * @b: second int
+ * return: subraction final result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - mod of two integers
+ * @a: first int
+ * @b: second int
+ * return: final answer
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * return: multiply
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div -divide two integers
+ * @a: first int
+ * @b: second int
+ * return: division result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
